fea99b15452af0f539a79a90da2a8e874678329e
Danucas <danrodcastillo1994@gmail.com>
2025-03-28 07:38:32 -0500

    remove unused imports

--- hacku_test/notes/views.py
+++ hacku_test/notes/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.http import HttpResponse, HttpResponseRedirect
-from django.views.generic.list import ListView
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from . import models
 from . import forms
 
